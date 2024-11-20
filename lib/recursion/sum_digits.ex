7e6d92dcb1abf3cab29a5a48d9b944db536bf343
Pedro Botega <botegapedro@gmail.com>
2024-11-20 15:12:42 +0000

    Recursion sum impproved

--- lib/recursion/sum_digits.ex
+++ lib/recursion/sum_digits.ex
@@ -1,13 +1,8 @@
 defmodule Tutorials.Recursion.SumDigits do
-  def upto(0), do: 0
+  def upto(num, acc \\ 0)
+  def upto(0, acc), do: acc
 
-  def upto(num) do
-    num + upto(num - 1)
-  end
-
-  def upto_tail(0, acc \\ 0), do: acc
-
-  def upto_tail(num, acc) do
-    upto_tail(num - 1, acc + num)
+  def upto(num, acc) do
+    upto(num - 1, acc + num)
   end
 end
