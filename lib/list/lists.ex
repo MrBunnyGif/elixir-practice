875a3e971b777a296ca81b90988a2c491cab5090
Pedro Botega <botegapedro@gmail.com>
2024-11-21 15:20:54 +0000

    reverse list

--- lib/list/lists.ex
+++ lib/list/lists.ex
@@ -1,37 +1,21 @@
 defmodule Tutorials.Lists do
-  @moduledoc """
-  Documentation for Tutorials.Lists.
-  """
-
-  @doc """
-  Sums the elements of a list using recursion.
-
-  ## Examples
-
-      iex> Tutorials.Lists.sum([1, 2, 3])
-      6
-
-  """
   def sum([]), do: 0
 
   def sum([h | t]) do
     h + sum(t)
   end
 
-  @doc """
-  Sums the elements of a list using recursion.
-
-  ## Examples
-
-      iex> Tutorials.Lists.sum([1, 2, 3])
-      6
-
-  """
-  @spec sum_recursive([integer]) :: integer
   def sum_recursive(num, acc \\ 0)
   def sum_recursive([], acc), do: acc
 
   def sum_recursive([h | t], acc) do
     sum_recursive(t, acc + h)
+  end
+
+  def reverse(elements, acc \\ [])
+  def reverse([], acc), do: acc
+
+  def reverse([h | t], acc) do
+    reverse(t, [h | acc])
   end
 end
