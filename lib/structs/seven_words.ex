42639a6c06d7a5272cf0c1440607e6b950e4309a
Pedro Botega <botegapedro@gmail.com>
2024-11-22 09:53:12 +0000

    Enum.each

---
+++ lib/structs/seven_words.ex
@@ -0,0 +1,28 @@
+defmodule Tutorials.Structs.SevenWonders do
+  defstruct name: "", country: ""
+  alias Tutorials.Structs.SevenWonders
+
+  @type t :: %SevenWonders{
+          name: String.t(),
+          country: String.t()
+        }
+
+  @spec all() :: [t()]
+  def all() do
+    [
+      %SevenWonders{name: "The Great Wall of China", country: "China"},
+      %SevenWonders{name: "The Taj Mahal", country: "India"},
+      %SevenWonders{name: "Machu Picchu", country: "Peru"},
+      %SevenWonders{name: "Chich n Itz ", country: "Mexico"},
+      %SevenWonders{name: "The Roman Colosseum", country: "Italy"},
+      %SevenWonders{name: "The Christ the Redeemer statue", country: "Brazil"},
+      %SevenWonders{name: "The Pyramids of Giza", country: "Egypt"}
+    ]
+  end
+
+  @spec print_names([t()]) :: :ok
+  def print_names(wonders) do
+    wonders
+    |> Enum.each(fn %{name: name} -> IO.puts(name) end)
+  end
+end
